f39ef3eff4ec24ef723983c61cd6eb0f73279ea5
hngieng9@gmail.com <hngieng9@gmail.com>
2023-01-05 16:44:31 +0800

    debugging

--- frontend/src/containers/Leaderboard.js
+++ frontend/src/containers/Leaderboard.js
@@ -86,7 +86,7 @@ const Leaderboard = () => {
     }
 
     useEffect(() => {
-        if(auth().name)
+        if(auth().name !== undefined)
         {
             console.log(auth().name)
             getUsers();
